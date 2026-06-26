f345152f0d46a410152d46ea86bf8167d8037adb
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-06-27 01:00:16 +0600

    x

--- src/services/imagekit.js
+++
@@ -1,9 +0,0 @@
-import { ImageKit } from "@imagekit/nodejs";
-
-const imagekit = new ImageKit({
-    publicKey: process.env.PUBLIC_KEY,
-    privateKey: process.env.PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGE_URL,
-});
-
-export default imagekit;
